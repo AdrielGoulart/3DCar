285a1b6fd336d518e01744aa01901fc6732b8304
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2019-10-16 10:31:35 -0300

    Adicionando a primeira parte da curva

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ function desenhar() {
 }
 
 requestAnimationFrame(desenhar);*/
+var materialLinha = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
+var geometriaLinha = new THREE.Geometry();
 var cena = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(1, 0.09, 18);
@@ -90,6 +92,38 @@ plane.position.set(1, 0, 0);
 
 // Adicionando o plano a cena
 cena.add(plane);
+//Spline 
+var curva = new THREE.SplineCurve([
+  new THREE.Vector3(-7.2, 3, 0.2),
+  new THREE.Vector3(-7.2, 7.5, 0.2),
+  new THREE.Vector3(-4, 7.5, 0.2),
+  new THREE.Vector3(-3.5, 3, 0.2),
+  new THREE.Vector3(2, 3, 0.2),
+  new THREE.Vector3(3, 7.5, 0.2),
+  new THREE.Vector3(5.5, 7.5, 0.2),
+  new THREE.Vector3(6.5, 1.5, 0.2),
+  new THREE.Vector3(8.7, 0, 0.2),
+  new THREE.Vector3(9, -3, 0.2),
+  new THREE.Vector3(7, -4.5, 0.2)
+]);
+
+var caminho = new THREE.Path(curva.getPoints(50));
+var geometriaLinha = caminho.createPointsGeometry(50);
+
+//Desenhar os pontos de referencia 
+var materialPonto = new THREE.PointsMaterial({ size: 10, sizeAttenuation: false });
+//Outra forma de ver os pontos
+//for (let p of geometriaLinha.vertices) {
+for (let p of curva.points) {
+  var geometriaPonto = new THREE.Geometry();
+  geometriaPonto.vertices.push(new THREE.Vector3(p.x, p.y, p.z));
+  var ponto = new THREE.Points(geometriaPonto, materialPonto);
+  cena.add(ponto);
+}
+
+
+var linha = new THREE.Line(geometriaLinha, materialLinha);
+cena.add(linha);
 
 render();
 
