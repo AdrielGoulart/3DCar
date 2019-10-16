b2c41e33b5762d1385a09cb5103d3f938e29a568
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2019-10-16 15:43:49 -0300

    Movimentação do carro implementado

--- script.js
+++ script.js
@@ -1,98 +1,48 @@
 "use strict";
-/*var cena = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
-var render = new THREE.WebGLRenderer();
-render.setSize(window.innerWidth, window.innerHeight);
-var canvas = render.domElement;
-document.body.appendChild(canvas);
+//Linha da pista
 var materialLinha = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
 var geometriaLinha = new THREE.Geometry();
 
-
-var material = new THREE.MeshBasicMaterial({
-   // map : THREE.ImageUtils.loadTexture('pista.png'),  // if I put only this, I see no mesh
-   color : 0x0000ff    // this draws blue mesh
-});
-
-// plane
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), material);
-
-cena.add( plane );
-
-//Spline 
-var curva = new THREE.SplineCurve([
-    new THREE.Vector3(-1, 0, 0),
-    new THREE.Vector3(-0.5, 0.5, 0),
-    new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(0.5, -0.5, 0),
-    new THREE.Vector3(1, 0, 0),
-    new THREE.Vector3(1.5, 0.5, 0),
-    new THREE.Vector3(2, 0, 0)
-]);
-
-var caminho = new THREE.Path(curva.getPoints(50));
-var geometriaLinha = caminho.createPointsGeometry(50);
-
-//Desenhar os pontos de referencia 
-var materialPonto = new THREE.PointsMaterial({ size: 10, sizeAttenuation: false });
-//Outra forma de ver os pontos
-//for (let p of geometriaLinha.vertices) {
-for (let p of curva.points) {
-    var geometriaPonto = new THREE.Geometry();
-    geometriaPonto.vertices.push(new THREE.Vector3(p.x, p.y, p.z));
-    var ponto = new THREE.Points(geometriaPonto, materialPonto);
-    cena.add(ponto);
-}
-
-
-var linha = new THREE.Line(geometriaLinha, materialLinha);
-cena.add(linha);
-camera.position.z = 5;
-
-function desenhar() {
-    curva.points[0].y -= 0.01;
-    //curva.points[1].x -= 0.05;
-    var caminho = new THREE.Path(curva.getPoints(50));
-    linha.geometry = caminho.createPointsGeometry(50);
-    render.render(cena, camera);
-   
-    requestAnimationFrame(desenhar);
-}
-
-requestAnimationFrame(desenhar);*/
-var materialLinha = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
-var geometriaLinha = new THREE.Geometry();
+//Cena
 var cena = new THREE.Scene();
+
+//Câmera
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+
+//Posição da câmera
 camera.position.set(1, 0.09, 18);
+
+//Renderizador
 var renderer = new THREE.WebGLRenderer({
   antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-var canvas = renderer.domElement;
-document.body.appendChild(canvas);
 renderer.setClearColor(0x101010);
 document.body.appendChild(renderer.domElement);
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
+//Controles da câmera
+var controles = new THREE.OrbitControls(camera, renderer.domElement);
 
-var light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.setScalar(10);
-cena.add(light);
+//Luz da câmera
+var luz = new THREE.DirectionalLight(0xffffff, 1);
+luz.position.setScalar(10);
+cena.add(luz);
 
-var planeGeometry = new THREE.PlaneGeometry(20, 20, 1, 1);
-var texture = new THREE.TextureLoader().load('pista.png');
-var planeMaterial = new THREE.MeshLambertMaterial({
-  map: texture
+//Pista
+var planoPista = new THREE.PlaneGeometry(20, 20, 1, 1);
+var pistaMaterial = new THREE.MeshLambertMaterial({
+  map: new THREE.TextureLoader().load('pista.png')
 });
+//Unindo o material da pista com o plano
+var material = new THREE.Mesh(planoPista, pistaMaterial);
+material.receiveShadow = true;
+material.position.set(1, 0, 0);
 
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.receiveShadow = true;
-plane.position.set(1, 0, 0);
+// Adicionando o a pista a cena
+cena.add(material);
 
-// Adicionando o plano a cena
-cena.add(plane);
 //Spline 
+//Pontos por onde a curva irá passar
 var curva = new THREE.SplineCurve([
   new THREE.Vector3(-7.2, 3, 0.2),
   new THREE.Vector3(-7.2, 7.5, 0.2),
@@ -112,8 +62,8 @@ var geometriaLinha = caminho.createPointsGeometry(50);
 
 //Desenhar os pontos de referencia 
 var materialPonto = new THREE.PointsMaterial({ size: 10, sizeAttenuation: false });
-//Outra forma de ver os pontos
-//for (let p of geometriaLinha.vertices) {
+
+//For responsáve por adicionar os pontos na tela
 for (let p of curva.points) {
   var geometriaPonto = new THREE.Geometry();
   geometriaPonto.vertices.push(new THREE.Vector3(p.x, p.y, p.z));
@@ -121,23 +71,56 @@ for (let p of curva.points) {
   cena.add(ponto);
 }
 
-
+//Linha que será o caminho por onde o carro irá passar
 var linha = new THREE.Line(geometriaLinha, materialLinha);
 cena.add(linha);
 
-render();
+//Cubo
+var geometria = new THREE.BoxGeometry(1, 1.8, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cubo = new THREE.Mesh(geometria, material);
+cubo.position.set(-7.2, 3, 0.2);
+cena.add(cubo);
 
-function render() {
-  requestAnimationFrame(render);
+//Pontos por onde o carro irá passar
+var p1 = new TWEEN.Tween(cubo.position).to(curva.points[0], 1000);
+var p2 = new TWEEN.Tween(cubo.position).to(curva.points[1], 1000);
+var p3 = new TWEEN.Tween(cubo.position).to(curva.points[2], 1000);
+var p4 = new TWEEN.Tween(cubo.position).to(curva.points[3], 1000);
+var p5 = new TWEEN.Tween(cubo.position).to(curva.points[4], 1000);
+var p6 = new TWEEN.Tween(cubo.position).to(curva.points[5], 1000);
+var p7 = new TWEEN.Tween(cubo.position).to(curva.points[6], 1000);
+var p8 = new TWEEN.Tween(cubo.position).to(curva.points[7], 1000);
+var p9 = new TWEEN.Tween(cubo.position).to(curva.points[8], 1000);
+var p10 = new TWEEN.Tween(cubo.position).to(curva.points[9], 1000);
+var p11 = new TWEEN.Tween(cubo.position).to(curva.points[10], 1000);
+
+//Ordem dos pontos que o carro irá passar
+p1.chain(p2);
+p2.chain(p3);
+p3.chain(p4);
+p4.chain(p5);
+p5.chain(p6);
+p6.chain(p7);
+p7.chain(p8);
+p8.chain(p9);
+p9.chain(p10);
+p10.chain(p11);
+
+//Ponto de inicio
+p1.start();
+
+function desenhar() {
+  animacao();
+  requestAnimationFrame(desenhar);
   renderer.render(cena, camera);
 }
 
-canvas.addEventListener("mousemove", function (e) 
-{
-    if(e.buttons > 0)
-    {
-      //FIXME
-      //plane.positiion.x = -0.5 * Math.PI;
-      camera.positiion.x = -0.5 * Math.PI;
-    }
-}, false);
+desenhar();
+
+//Método responsável por fazer o movimento do carro de um vértice a outro
+function animacao(tempo) {
+  requestAnimationFrame(animacao);
+  TWEEN.update(tempo);
+  renderer.render(cena, camera);
+}
